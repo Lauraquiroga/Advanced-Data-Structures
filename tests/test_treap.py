abd9538ae6d7013a559190125ecf7be278eb1d71
Lauraquiroga <lauragq10@hotmail.com>
2025-02-28 14:59:27 -0800

    Initialize tests

---
+++ tests/test_treap.py
@@ -0,0 +1,8 @@
+import unittest
+import random
+from src.treap import Treap
+
+class TestTreap(unittest.TestCase):
+    """
+    Unit test class for the `Treap` class, using Python's built-in unittest framework.
+    """
